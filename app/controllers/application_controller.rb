13ba6fa5e5a068193b953125931a30549daff086
Mário Marroquim <mariomarroquim@ufpi.edu.br>
2026-01-10 10:35:19 -0300

    Bump version and set HTTP cache update after version bumps

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,6 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
-  etag { APP_VERSION  }
+  # Invalidate caches when the app version changes.
+  etag { APP_VERSION }
 end
